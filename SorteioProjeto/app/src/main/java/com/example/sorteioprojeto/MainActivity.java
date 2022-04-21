65302703cc6372bf95fe00fb0b29ff2285acef3d
João Marcos Pimenta Martins <joaomarcos-icm@hotmail.com>
2022-04-21 13:21:40 -0300

    Initial commit

---
+++ SorteioProjeto/app/src/main/java/com/example/sorteioprojeto/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.sorteioprojeto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeNum(View view){
+        TextView texto = findViewById(R.id.numberNow);
+
+        int number = new Random().nextInt(11);
+
+
+        texto.setText("Numero: " + number);
+    }
+
+}
